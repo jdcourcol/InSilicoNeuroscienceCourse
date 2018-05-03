7f6f252a7ecf34ef53e3b09311f5ff6cacb6a187
jean-denis courcol <jean-denis.courcol@epfl.ch>
2018-05-03 14:18:05 +0200

    offspring size 2

--- nsg-portal-course/build.sh
+++ nsg-portal-course/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 tar -cvf ./model/initial_data.tar.gz ./model/hippo-ca1
+
+tar -cvf ./model/initial_ncx_data.tar.gz ./model/neoctx-L5PC
 pushd ./utils/nsg-bluepyopt-job/
 python setup.py sdist
 popd
--- nsg-portal-course/model/neoctx-L5pc/init.py
+++ nsg-portal-course/model/neoctx-L5pc/init.py
@@ -1,2 +1,2 @@
 import os
-os.system('python opt_neuron.py --max_ngen=20 --offspring_size=20 --start --checkpoint ./checkpoints/checkpoint.pkl')
+os.system('python opt_neuron.py --max_ngen=2 --offspring_size=2 --start --checkpoint ./checkpoints/checkpoint.pkl')
