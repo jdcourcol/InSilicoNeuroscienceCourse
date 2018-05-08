425885f6cb6498f4900336173b07aca87df994eb
jean-denis courcol <jean-denis.courcol@epfl.ch>
2018-05-08 13:10:47 +0200

    remove subdirs

--- nsg-portal-course/build.sh
+++ nsg-portal-course/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-tar -cvf ./model/initial_data.tar.gz ./model/hippo-ca1
 
-tar -cvf ./model/initial_ncx_data.tar.gz ./model/neoctx-L5PC
+tar -cvf ./initial_ncx_data.tar.gz ./model/
 pushd ./utils/nsg-bluepyopt-job/
 python setup.py sdist
 popd
--- nsg-portal-course/model/neoctx-L5pc/init.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system('python opt_neuron.py --max_ngen=2 --offspring_size=2 --start --checkpoint ./checkpoints/checkpoint.pkl')
